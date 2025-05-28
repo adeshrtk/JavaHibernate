65f15f68e7d1cb411b821ed00f24d11535106147
adeshrtk <adeshnyc2018@hotmail.com>
2025-05-28 20:06:16 +0530

    get() and load() methods

--- JavaHibernate/src/main/java/com/javatut/connection/FetchDemo.java
+++ JavaHibernate/src/main/java/com/javatut/connection/FetchDemo.java
@@ -15,12 +15,29 @@ public class FetchDemo {
 		
 		Session session = factory.openSession();
 		
-		//get address:1
+		//get address when object exists
 		Address ad = (Address) session.get(Address.class, 2);
-		System.out.println(ad);
+		System.out.println(ad.getStreet());
+		
+		Address ad5 = (Address) session.get(Address.class, 3);
+		System.out.println(ad5.getStreet());
+		
+		//get address when object does not exists. get() will return null
+		Address ad1 = (Address) session.get(Address.class, 5);
+		System.out.println(ad1);
+		
+//		//load address when object exists
+		Address ad3 = (Address) session.load(Address.class, 2);
+		System.out.println(ad3);
+		
+		//load address when object doen't exist. It will throw ObjectNotFound error
+//		Address ad4 = (Address)session.load(Address.class, 5);
+//		System.out.println(ad4);
 		
 		session.close();
 		factory.close();
+		
+		System.out.println("Record Fetched Successfully");
 	}
 
 }
